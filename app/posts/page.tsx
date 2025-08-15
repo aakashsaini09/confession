5a424ccadca8b5d73ea472f6a3633d648d42492a
aakashsaini09 <aakashsaini948585@gmail.com>
2025-08-15 21:12:37 +0530

    fixing structure

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,24 @@
 "use client"
-import React, { useState } from 'react'
-import HomePage from '@/components/Home'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import axios from 'axios'
+import Navbar from '@/components/Navbar'
+import Card from '@/components/ui/card'
+import { Confession } from '@/types'
 
 const Page = () => {
     const [message, setmessage] = useState("")
     const [loading, setloading] = useState(false)
+    const [confession, setconfession] = useState<Confession[]>([])
+       const getAll = async () => {
+           setloading(true)
+           const res = await axios.get("/api/confessions");
+           setconfession(res.data)
+           setloading(false)
+       };
+       useEffect(() => {
+         getAll()
+       }, [])
     const newPost = async () => {
         setloading(true)
     await axios.post("/api/add", {
@@ -17,12 +29,15 @@ const Page = () => {
         }
     });
     alert("Posted!!")
+    getAll()
     setmessage("")
     setloading(false)
 };
   return (
-    <div>
-      <HomePage/>
+    <>
+    <Navbar/> 
+    <div className='px-20 py-10'>
+      
       <textarea 
         name="" 
         id=""
@@ -32,7 +47,19 @@ const Page = () => {
         onChange={(e)=> setmessage(e.target.value)}
     />
       <Button disabled={loading} className='' onClick={newPost}>Click to Post</Button>
+      <div>
+        <div className='grid sm:grid-cols-1 md:grid-cols-3 gap-4'>
+            {confession ? confession.map((con, index) => {
+                return (
+                    <div key={index}>
+                        <Card data={con}/>
+                    </div>
+                );
+            }): <div>loading</div>}
+        </div>
+        </div>
     </div>
+    </>
   )
 }
 
